920d1e4194fdf6da4af69337e7e649f2a4865e92
Mallusbh <134638583+Mallusbh@users.noreply.github.com>
2023-06-21 12:30:23 +0530

    Update EmployeeService.java

--- src/main/java/com/mt/services/EmployeeService.java
+++ src/main/java/com/mt/services/EmployeeService.java
@@ -22,7 +22,7 @@ public class EmployeeService {
 			throws JSONException {
 
 		JSONObject js = new JSONObject();
-		js.put("Name", "Mallu Technologies");
+		js.put("Name", "Mallu Prince");
 		js.put("Calling Name", "Mithun");
 		js.put("DOB", "08-Nov-2011");
 		js.put("Hobbies", "Reading Technical Blogs,Teaching, Helping to Poor People..");
